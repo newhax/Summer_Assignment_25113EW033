411e18d95fe179086b804a8c57ba1a7a0bbd0373
newhax <106071222+newhax@users.noreply.github.com>
2026-05-28 23:40:41 +0530

    Write a program to Print multiplication table of  a given number.

---
+++ question2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class question2 {
+    public static void main(String[] args) {
+        
+        //to print tht multiplication table.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + n*i);
+        }
+    }
+}
