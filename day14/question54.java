2e78d93948609300b7482c8c17263e7774570a98
newhax <106071222+newhax@users.noreply.github.com>
2026-06-14 21:23:13 +0530

    Write a program to Frequency of an element.

---
+++ day14/question54.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class question54 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to search: ");
+        int key = sc.nextInt();
+
+        int count=0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == key) {
+                count++;
+            
+            }
+        }
+
+        if(count!=0)
+            System.out.println("Element's frequency in Array: " + count);
+        else
+            System.out.println("Element not found");
+
+        sc.close();
+    }
+}
