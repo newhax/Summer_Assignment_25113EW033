a0524bdbe3bfd66138af1998aa0d7a0b00d4d601
newhax <106071222+newhax@users.noreply.github.com>
2026-06-14 11:54:50 +0530

    Write a program to Count even and odd  elements.

---
+++ day13/question52.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class question52 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        int even = 0, odd = 0;
+
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if(arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Even Elements = " + even);
+        System.out.println("Odd Elements = " + odd);
+
+        sc.close();
+    }
+}
