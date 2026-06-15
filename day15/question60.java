cd6297e40d0bc9276f4296694a3ac90d4b868815
Abhinav Srivastava <106071222+newhax@users.noreply.github.com>
2026-06-15 20:27:24 +0530

    Write a program to Move zeroes to end.

---
+++ day15/question60.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class question60 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int index = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        while(index < n) {
+            arr[index] = 0;
+            index++;
+        }
+
+        System.out.println("Array after moving zeroes:");
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
