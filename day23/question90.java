49f854daedda2ab71cee3e26a2bb58367f236d42
Abhinav Srivastava <106071222+newhax@users.noreply.github.com>
2026-06-23 12:09:19 +0530

    Write a program to Find first repeating  character.

---
+++ day23/question90.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class question90 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        
+
+        for (int i = 0; i < str.length(); i++) {
+           int count=0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                
+                }
+            }
+        
+            if (count>1){
+                System.out.println("First Repeating"+str.charAt(i));
+                break;
+            }
+            
+        }
+
+        sc.close();
+    }
+}
